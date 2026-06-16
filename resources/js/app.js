8bf2893b0a5ddd12c1ad6f050d865ea3100766ae
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 06:49:37 -0300

    :recycle: refactor: atualizando resources/js/app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-//
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+document.addEventListener('livewire:init', () => {
+    Livewire.on('notify', ({ type = 'info', title = '', text = '' }) => {
+        Swal.fire({
+            icon: type,
+            title,
+            text,
+            confirmButtonColor: '#0f4c81',
+            background: '#ffffff',
+            color: '#0f172a',
+        });
+    });
+
+    Livewire.on('scroll-top', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    });
+});
