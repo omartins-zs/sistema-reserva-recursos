10cde0d88883ffd623e717dc5d3a7d497ca10c7f
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:54 -0300

    :sparkles: feat: adicionando create-record

---
+++ lang/vendor/filament-panels/pt_BR/resources/pages/create-record.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'title' => 'Criar :label',
+    'breadcrumb' => 'Criar',
+    'form' => [
+        'actions' => [
+            'cancel' => [
+                'label' => 'Cancelar',
+            ],
+            'create' => [
+                'label' => 'Criar',
+            ],
+            'create_another' => [
+                'label' => 'Criar e cadastrar outro',
+            ],
+        ],
+    ],
+    'notifications' => [
+        'created' => [
+            'title' => 'Cadastro realizado',
+        ],
+    ],
+];
