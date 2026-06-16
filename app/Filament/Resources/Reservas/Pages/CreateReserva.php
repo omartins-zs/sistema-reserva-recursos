308b8d8791f7e271e079aaf8477c1807c502fbd4
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:36 -0300

    :recycle: refactor: atualizando createreserva

--- app/Filament/Resources/Reservas/Pages/CreateReserva.php
+++ app/Filament/Resources/Reservas/Pages/CreateReserva.php
@@ -3,9 +3,35 @@
 namespace App\Filament\Resources\Reservas\Pages;
 
 use App\Filament\Resources\Reservas\ReservaResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateReserva extends CreateRecord
 {
     protected static string $resource = ReservaResource::class;
+
+    protected static ?string $title = 'Nova reserva interna';
+
+    protected static ?string $breadcrumb = 'Nova reserva';
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Reserva cadastrada com sucesso.';
+    }
+
+    /**
+     * @return array<Action>
+     */
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction()->label('Criar reserva'),
+            $this->getCreateAnotherFormAction()->label('Criar e cadastrar outra'),
+        ];
+    }
 }
