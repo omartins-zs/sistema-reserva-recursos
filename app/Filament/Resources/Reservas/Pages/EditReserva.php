4d2d7c53f9e98f62efd254852860ceb597bca909
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:37 -0300

    :recycle: refactor: atualizando editreserva

--- app/Filament/Resources/Reservas/Pages/EditReserva.php
+++ app/Filament/Resources/Reservas/Pages/EditReserva.php
@@ -10,10 +10,20 @@ class EditReserva extends EditRecord
 {
     protected static string $resource = ReservaResource::class;
 
+    protected static ?string $title = 'Detalhes da reserva';
+
+    protected static ?string $breadcrumb = 'Detalhes';
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Reserva atualizada com sucesso.';
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            DeleteAction::make()
+                ->label('Excluir reserva'),
         ];
     }
 }
