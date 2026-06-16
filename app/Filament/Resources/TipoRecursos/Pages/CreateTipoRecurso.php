5afc4a90e464681d5834c3afd9855be14f8656a2
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 06:49:41 -0300

    :sparkles: feat: adicionando createtiporecurso

---
+++ app/Filament/Resources/TipoRecursos/Pages/CreateTipoRecurso.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\TipoRecursos\Pages;
+
+use App\Filament\Resources\TipoRecursos\TipoRecursoResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTipoRecurso extends CreateRecord
+{
+    protected static string $resource = TipoRecursoResource::class;
+}
