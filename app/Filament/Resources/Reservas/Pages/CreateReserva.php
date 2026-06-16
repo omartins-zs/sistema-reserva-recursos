3657b323fa701f3bbd50ad55f3191e59d637a965
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 06:49:40 -0300

    :sparkles: feat: adicionando createreserva

---
+++ app/Filament/Resources/Reservas/Pages/CreateReserva.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Reservas\Pages;
+
+use App\Filament\Resources\Reservas\ReservaResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateReserva extends CreateRecord
+{
+    protected static string $resource = ReservaResource::class;
+}
