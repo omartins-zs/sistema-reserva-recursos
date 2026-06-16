3d0cb74c62e4626b07748b3044003f62f07d2c20
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 06:49:43 -0300

    :sparkles: feat: adicionando userstable

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nome')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('role')
+                    ->label('Perfil')
+                    ->badge()
+                    ->formatStateUsing(fn ($state) => $state->label()),
+                TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->dateTime('d/m/Y H:i'),
+            ])
+            ->filters([
+                SelectFilter::make('role')
+                    ->options([
+                        'administrador' => 'Administrador',
+                        'rh' => 'RH',
+                        'ti' => 'TI',
+                        'facilities' => 'Facilities',
+                        'colaborador' => 'Colaborador',
+                    ]),
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
