1871ae6b5a4f40a4b85bae3e05a78e384a2c8b68
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:53 -0300

    :sparkles: feat: adicionando create

---
+++ lang/vendor/filament-actions/pt_BR/create.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'single' => [
+        'label' => 'Novo :label',
+        'modal' => [
+            'heading' => 'Criar :label',
+            'actions' => [
+                'create' => [
+                    'label' => 'Criar',
+                ],
+                'create_another' => [
+                    'label' => 'Criar e cadastrar outro',
+                ],
+            ],
+        ],
+        'notifications' => [
+            'created' => [
+                'title' => 'Cadastro realizado',
+            ],
+        ],
+    ],
+];
