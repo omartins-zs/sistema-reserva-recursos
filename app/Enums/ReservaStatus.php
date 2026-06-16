0cc0550961ac2de15810bbe78e3462b4b92cb149
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:35 -0300

    :card_file_box: data: atualizando enum reservas...

--- app/Enums/ReservaStatus.php
+++ app/Enums/ReservaStatus.php
@@ -4,14 +4,18 @@ namespace App\Enums;
 
 enum ReservaStatus: string
 {
+    case PENDENTE_APROVACAO = 'pendente_aprovacao';
     case CONFIRMADO = 'confirmado';
+    case REJEITADO = 'rejeitado';
     case CANCELADO = 'cancelado';
     case FINALIZADO = 'finalizado';
 
     public function label(): string
     {
         return match ($this) {
+            self::PENDENTE_APROVACAO => 'Pendente de aprovacao',
             self::CONFIRMADO => 'Confirmado',
+            self::REJEITADO => 'Rejeitado',
             self::CANCELADO => 'Cancelado',
             self::FINALIZADO => 'Finalizado',
         };
@@ -20,9 +24,22 @@ enum ReservaStatus: string
     public function color(): string
     {
         return match ($this) {
+            self::PENDENTE_APROVACAO => 'warning',
             self::CONFIRMADO => 'success',
+            self::REJEITADO => 'danger',
             self::CANCELADO => 'danger',
             self::FINALIZADO => 'gray',
         };
+    }
+
+    /**
+     * @return list<string>
+     */
+    public static function bloqueiaAgenda(): array
+    {
+        return [
+            self::PENDENTE_APROVACAO->value,
+            self::CONFIRMADO->value,
+        ];
     }
 }
