43da4bb18e3a80dba4d623a31e522c599fdf58b5
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:54 -0300

    :sparkles: feat: adicionando edit-record

---
+++ lang/vendor/filament-panels/pt_BR/resources/pages/edit-record.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'title' => 'Editar :label',
+    'breadcrumb' => 'Editar',
+    'navigation_label' => 'Editar',
+    'form' => [
+        'actions' => [
+            'cancel' => [
+                'label' => 'Cancelar',
+            ],
+            'save' => [
+                'label' => 'Salvar alteracoes',
+            ],
+        ],
+    ],
+    'content' => [
+        'tab' => [
+            'label' => 'Editar',
+        ],
+    ],
+    'notifications' => [
+        'saved' => [
+            'title' => 'Alteracoes salvas',
+        ],
+    ],
+];
