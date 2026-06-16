93d89b9151316f1bc5f1189f5896c408fc493fd4
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 06:49:37 -0300

    :test_tube: test: atualizando testes exampletest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,22 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_public_home_page_loads_the_reservation_interface(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response
+            ->assertOk()
+            ->assertSee('Reserva de recursos corporativos');
     }
 }
