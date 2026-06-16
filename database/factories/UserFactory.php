c53bb15439708442adb22ee30ed445c46dd1d699
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 18:52:02 -0300

    :card_file_box: data: atualizando factory userf...

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'role' => fake()->randomElement(UserRole::cases()),
+            'departamento_id' => null,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
