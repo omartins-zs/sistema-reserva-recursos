2de8164822719918a0b1fec9349e68de77dcefcc
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 15:54:54 -0300

    :sparkles: feat: adicionando edit

---
+++ lang/vendor/filament-actions/pt_BR/edit.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'single' => [
+        'label' => 'Editar',
+        'modal' => [
+            'heading' => 'Editar :label',
+            'actions' => [
+                'save' => [
+                    'label' => 'Salvar alteracoes',
+                ],
+            ],
+        ],
+        'notifications' => [
+            'saved' => [
+                'title' => 'Alteracoes salvas',
+            ],
+        ],
+    ],
+];
