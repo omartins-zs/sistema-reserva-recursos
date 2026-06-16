1fdf3c18cb08c78a22dd69ee9325655386676605
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 06:49:39 -0300

    :sparkles: feat: adicionando createrecurso

---
+++ app/Filament/Resources/Recursos/Pages/CreateRecurso.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Recursos\Pages;
+
+use App\Filament\Resources\Recursos\RecursoResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRecurso extends CreateRecord
+{
+    protected static string $resource = RecursoResource::class;
+}
