15421a79fa28663bc06c0d4f3c1358d812f39908
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-16 18:52:02 -0300

    :card_file_box: data: atualizando seeder databa...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TiposRecursosSeeder::class,
             RecursosSeeder::class,
+            DepartamentosSeeder::class,
             UsersSeeder::class,
         ]);
     }
