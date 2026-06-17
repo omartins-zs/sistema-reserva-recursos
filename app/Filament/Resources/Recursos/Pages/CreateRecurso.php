a334c6179fafc734aa1a742ce9cd06101ff7f006
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-17 13:36:18 -0300

    :recycle: refactor: atualizando createrecurso

--- app/Filament/Resources/Recursos/Pages/CreateRecurso.php
+++ app/Filament/Resources/Recursos/Pages/CreateRecurso.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRecurso extends CreateRecord
 {
     protected static string $resource = RecursoResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('edit', ['record' => $this->getRecord()]);
+    }
 }
