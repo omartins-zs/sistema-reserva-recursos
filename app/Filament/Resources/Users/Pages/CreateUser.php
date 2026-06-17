837a8ed2f727975e982bdfcda1cf7f2d223f988f
omartins-zs <gabrielmatheus209@gmail.com>
2026-06-17 13:36:18 -0300

    :recycle: refactor: atualizando createuser

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('edit', ['record' => $this->getRecord()]);
+    }
 }
